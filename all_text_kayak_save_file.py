9fc6739a0e7b8453c273cb2e13cf65b95de640df
ebtrader <jsiddique@gmail.com>
2023-03-28 09:27:01 -0400

    Commit

---
+++ all_text_kayak_save_file.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+
+# https://www.geeksforgeeks.org/get-all-text-of-the-page-using-selenium-in-python/
+
+options = webdriver.ChromeOptions()
+
+driver = webdriver.Chrome(options=options)
+
+# Target URL
+driver.get('https://www.kayak.com/flights/JFK-SFO/2023-05-25/2023-06-03?sort=bestflight_a')
+# To load entire webpage
+time.sleep(5)
+
+text_file = driver.find_element("xpath", "/html/body").text
+
+file = open('kayak.txt', 'w')
+
+file.write(text_file)
+
+file.close()
+
+# Printing the whole body text
+print(text_file)
+
+# Closing the driver
+driver.close()
+
