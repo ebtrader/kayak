252857f4455c67072097d9ffe88d329490b28e5e
ebtrader <jsiddique@gmail.com>
2023-03-28 09:47:56 -0400

    Commit

--- all_text_kayak_save_file.py
+++ all_text_kayak_save_file.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome(options=options)
 # Target URL
 driver.get('https://www.kayak.com/flights/JFK-SFO/2023-05-25/2023-06-03?sort=bestflight_a')
 # To load entire webpage
-time.sleep(5)
+time.sleep(10)
 
 text_file = driver.find_element("xpath", "/html/body").text
 
