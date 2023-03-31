3925a361a904de25e4aa0d4c31eb9209390bdeac
ebtrader <jsiddique@gmail.com>
2023-03-30 21:39:18 -0400

    Commit

--- kayak_parse_SLC.py
+++ kayak_parse_SLC.py
@@ -2,35 +2,35 @@ from selenium import webdriver
 import time
 import pandas as pd
 
-# # https://www.geeksforgeeks.org/get-all-text-of-the-page-using-selenium-in-python/
-#
-# options = webdriver.ChromeOptions()
-#
-# driver = webdriver.Chrome(options=options)
-#
-# # Target URL
-# driver.get('https://www.kayak.com/flights/EWR-DEN/2023-05-25/2023-06-03?sort=bestflight_a')
-# # To load entire webpage
-# time.sleep(15)
-#
-# text_file = driver.find_element("xpath", "/html/body").text
-#
-# file = open('kayak.txt', 'w', encoding='utf-8')
-#
-# file.write(text_file)
-#
-#
-# file.close()
-#
-#
-#
-# # Printing the whole body text
-# print(text_file)
-#
-# # Closing the driver
-# driver.close()
-#
-# # time.sleep(5)
+# https://www.geeksforgeeks.org/get-all-text-of-the-page-using-selenium-in-python/
+
+options = webdriver.ChromeOptions()
+
+driver = webdriver.Chrome(options=options)
+
+# Target URL
+driver.get('https://www.kayak.com/flights/EWR-SLC/2023-05-25/2023-06-03?sort=bestflight_a')
+# To load entire webpage
+time.sleep(15)
+
+text_file = driver.find_element("xpath", "/html/body").text
+
+file = open('kayak.txt', 'w', encoding='utf-8')
+
+file.write(text_file)
+
+
+file.close()
+
+
+
+# Printing the whole body text
+print(text_file)
+
+# Closing the driver
+driver.close()
+
+# time.sleep(5)
 
 df = pd.read_csv('kayak.txt', encoding='utf-8')
 df.reset_index(inplace=True)
@@ -95,5 +95,5 @@ for i in range(0, list_length):
 
 print(best_list)
 
-# df.to_csv('kayak_parsed.csv')
+df.to_csv('kayak_parsed1.csv')
 
